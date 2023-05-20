eb532fa1dfac433938e15ed596095520b49de62c
FlowersRlit <yourstrulygurl@gmail.com>
2023-05-21 05:38:34 +0700

    updated used controllers

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const Controller = require("../controllers/controller");
+const UserController = require("../controllers/UserController");
+const TanahController = require("../controllers/TanahController");
 
 const router = express.Router();
 
-router.get("/", Controller.helloWorld);
-router.get("/test", Controller.test);
+//user
+router.post("/user/registerNewUser", UserController.registerNewUser);
+
+//tanah
+router.get("/tanah/getListTanah", TanahController.getListTanah);
 
 module.exports = router;
